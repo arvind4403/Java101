42ecb2964bbffcde829fbc5aa154380cea6ea075
arvind4403 <67897982+arvind4403@users.noreply.github.com>
2023-06-11 23:56:22 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class SimpleCalculator {
+    public static void main(String[] args) {
+        char operator;
+        Double number1, number2, result;
+        Scanner input =new Scanner(System.in);
+        System.out.println("Choose an operator : + , - , * or % ");
+
+        operator = input.next().charAt(0);
+
+        System.out.println("Enter the first number ");
+        number1 = input.nextDouble();
+        System.out.println("Enter the second number");
+        number2 = input.nextDouble();
+
+        switch (operator){
+            case '+':
+                result = number1 + number2 ;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+            case '-':
+                result = number1 - number2 ;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+
+            case '*':
+                result = number1 * number2 ;
+                System.out.println(number1 + " 8 " + number2 + " = " + result);
+                break;
+
+            case '%':
+                result = number1 % number2 ;
+                System.out.println(number1 + " % " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Invalid operation");
+                break;
+
+        }
+        input.close();
+    }
+}
